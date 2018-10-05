2eefadd8367b3f07a00698ade593515caa5bd65d
Jonah Penne <jpenne@umaryland.edu>
2018-10-05 14:38:02 -0400

    Added Find it Fast & some homepage revisions

--- elm-template-assets/js/elm-masonry-initializer.js
+++ elm-template-assets/js/elm-masonry-initializer.js
@@ -1,10 +1,24 @@
-/*Initializes the MAsonry grid for full Elm topic feeds*/
 
-var $grid = $('.elm-feed-main').imagesLoaded( function() {
-  // init Masonry after all images have loaded
-  $grid.masonry({
-    itemSelector: '.elm-feed-listing',
-    percentPosition: true,
-    gutter: '#elm-feed-gutter-sizer'
-  });
+$( document ).ready(function() {
+
+	/*Initializes the MAsonry grid for full Elm topic feeds*/
+
+	var $grid = $('.elm-feed-main').imagesLoaded( function() {
+	  // init Masonry after all images have loaded
+	  $grid.masonry({
+	    itemSelector: '.elm-feed-listing',
+	    percentPosition: true,
+	    gutter: '#elm-feed-gutter-sizer'
+	  });
+	});
+
+	var $grid2 = $('.findit-landing-grid').imagesLoaded( function() {
+	  // init Masonry after all images have loaded
+	  $grid2.masonry({
+	    itemSelector: '.findit-listing',
+	    percentPosition: true,
+	    gutter: '#elm-findit-gutter-sizer'
+	  });
+	});
+
 });
