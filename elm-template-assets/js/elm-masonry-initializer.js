6eb9e8a5316fd96b77578478924d5f3af28099a2
Jonah Penne <jpenne@umaryland.edu>
2018-10-05 15:04:05 -0400

    Additional revisions for ressponsiveness, etc

--- elm-template-assets/js/elm-masonry-initializer.js
+++ elm-template-assets/js/elm-masonry-initializer.js
@@ -1,24 +1,46 @@
 
-$( document ).ready(function() {
+function initializeElmMasonry() {
 
-	/*Initializes the MAsonry grid for full Elm topic feeds*/
-
+	/*Initializes the Masonry grid for full Elm topic feeds*/
 	var $grid = $('.elm-feed-main').imagesLoaded( function() {
 	  // init Masonry after all images have loaded
 	  $grid.masonry({
 	    itemSelector: '.elm-feed-listing',
 	    percentPosition: true,
-	    gutter: '#elm-feed-gutter-sizer'
+	    gutter: '#elm-feed-gutter-sizer',
+	    transitionDuration: '0s'
 	  });
 	});
 
+
+	/*Initializes the Masonry grid for Find it Fast modules on landing pages*/
 	var $grid2 = $('.findit-landing-grid').imagesLoaded( function() {
 	  // init Masonry after all images have loaded
 	  $grid2.masonry({
 	    itemSelector: '.findit-listing',
 	    percentPosition: true,
-	    gutter: '#elm-findit-gutter-sizer'
+	    gutter: '#elm-findit-gutter-sizer',
+	    transitionDuration: '0s'
 	  });
 	});
 
+	/*Initializes the Masonry grid for Voices and Opinions modules on landing pages*/
+	var $grid3 = $('.voices-landing-grid').imagesLoaded( function() {
+	  // init Masonry after all images have loaded
+	  $grid3.masonry({
+	    itemSelector: '.elm-voice-op-listing',
+	    percentPosition: true,
+	    gutter: '#voices-landing-gutter-sizer',
+	    transitionDuration: '0s'
+	  });
+	});
+
+}
+
+$( window ).resize(function() {
+	initializeElmMasonry();
+});
+
+$(document).ready(function() {
+	initializeElmMasonry();
 });
